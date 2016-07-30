0cfbaa0166f3058e85fa3776db7917e5efe042c7
martincarmen03 <martincarmen03@gmail.com>
2016-07-30 09:22:51 -0700

    minor changes

--- app.js
+++
@@ -1,8 +0,0 @@
-window.onload = chooseImage;
-
-var myPix = new Array("images/wine_glass.jpg","images/boots.jpg","images/water_can.jpg");
-
-function chooseImage() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("imageHolder").src = myPix[randomNum];
-}
