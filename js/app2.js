ed16db7235950de367a8de6fa7e5099df43ae33a
Jon Walz <jon.walz.music@gmail.com>
2016-08-06 13:22:28 -0700

    Made username persist to local storage

--- js/app2.js
+++ js/app2.js
@@ -82,3 +82,16 @@ function checkVotes() {
 }
 
 getThreeImages();
+
+function greetUser() {
+  if (localStorage.getItem("username") == null) {
+    var username = prompt("Welcome to Bus Mall Vote Tracker!", "Enter Your Name");
+    localStorage.setItem("username", username);
+    document.getElementById("greeting").innerText = "Welcome "+username;
+  } else {
+    var username = localStorage.getItem("username");
+    document.getElementById("greeting").innerText = "Welcome Back "+username;
+  }
+}
+
+window.addEventListener("load", greetUser);
